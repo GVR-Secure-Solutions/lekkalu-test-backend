a6b4bd5119e967b6759a177e094979177775e278
SzatmariA <szatma@inf.u-szeged.hu>
2023-12-10 17:47:01 +0100

    add post loan from another user

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import unittest
 class LoanTests(unittest.TestCase):
     username = "szatma"
     password = "123456789"
+
     def test_get_loan_transactions_returns_200(self):
         URL = "https://api.finuncle.com/api/loan_transactions/"
-        response = requests.get(URL, auth=(self.username,self.password))
+        response = requests.get(URL, auth=(self.username, self.password))
         self.assertEqual(response.status_code, 200)
 
     def test_get_loan_by_id_by_other_user_should_return_401(self):
-
         URL = "http://api.finuncle.com/api/loans/1"
-        response = requests.get(URL, auth=(self.username,self.password))
+        response = requests.get(URL, auth=(self.username, self.password))
         self.assertEqual(response.status_code, 401)
 
+    def test_post_loan_by_id_by_other_user_should_return_401(self):
+        URL = "http://api.finuncle.com/api/loans/1"
+        response = requests.post(URL, auth=(self.username, self.password))
+        self.assertEqual(response.status_code, 401)
 
 if __name__ == "__main__":
     unittest.main()
